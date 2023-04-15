299f415f7d122ac821c60a5ff00c724a367ca2c2
Matvey Kottsov <contact@matvey.codes>
2023-04-15 19:50:26 +0300

    chore: uppercase for SQL commands

--- docs/inc/lst/init.sql
+++ docs/inc/lst/init.sql
@@ -7,127 +7,127 @@ CREATE TYPE user_status AS ENUM ('pending', 'active', 'blocked');
 CREATE DOMAIN phone_number AS TEXT CHECK (VALUE ~ '^[+]7[0-9]{10}$');
 
 CREATE TABLE IF NOT EXISTS users (
-    id uuid primary key default uuid_generate_v4(),
-    status user_status not null default 'pending',
-    date_joined timestamp not null default now() check (date_joined > birthdate),
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    status user_status NOT NULL DEFAULT 'pending',
+    date_joined timestamp NOT NULL DEFAULT now() CHECK (date_joined > birthdate),
     middle_name varchar(256),
     first_name varchar(256),
     last_name varchar(256),
     email citext,
-    phone phone_number unique not null,
-    birthdate date check (birthdate > '1930-01-01')
+    phone phone_number UNIQUE NOT NULL,
+    birthdate date CHECK (birthdate > '1930-01-01')
 );
 
 CREATE TABLE IF NOT EXISTS supports (
-    id uuid primary key default uuid_generate_v4(),
-    middle_name varchar(256) not null,
-    first_name varchar(256) not null,
-    last_name varchar(256) not null,
-    email citext not null,
-    phone phone_number unique not null
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    middle_name varchar(256) NOT NULL,
+    first_name varchar(256) NOT NULL,
+    last_name varchar(256) NOT NULL,
+    email citext NOT NULL,
+    phone phone_number UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS technicians (
-    id uuid primary key default uuid_generate_v4(),
-    middle_name varchar(256) not null,
-    first_name varchar(256) not null,
-    last_name varchar(256) not null,
-    email citext not null,
-    phone phone_number unique not null
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    middle_name varchar(256) NOT NULL,
+    first_name varchar(256) NOT NULL,
+    last_name varchar(256) NOT NULL,
+    email citext NOT NULL,
+    phone phone_number UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS admins (
-    id uuid primary key default uuid_generate_v4(),
-    middle_name varchar(256) not null,
-    first_name varchar(256) not null,
-    last_name varchar(256) not null,
-    email citext not null,
-    phone phone_number unique not null
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    middle_name varchar(256) NOT NULL,
+    first_name varchar(256) NOT NULL,
+    last_name varchar(256) NOT NULL,
+    email citext NOT NULL,
+    phone phone_number UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS scooter_manufacturers (
-    id uuid primary key default uuid_generate_v4(),
-    title varchar(256) not null
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    title varchar(256) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS scooter_models (
-    id uuid primary key default uuid_generate_v4(),
-    manufacturer_id uuid not null references scooter_manufacturers(id),
-    title varchar(256) not null,
-    single_charge_mileage integer not null check (single_charge_mileage > 0),
-    weight integer not null check (weight > 0),
-    max_speed integer not null check (max_speed > 0),
-    max_load integer not null check (max_load > 0),
-    year smallint not null check (year > 2000)
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    manufacturer_id uuid NOT NULL REFERENCES scooter_manufacturers(id),
+    title varchar(256) NOT NULL,
+    single_charge_mileage integer NOT NULL CHECK (single_charge_mileage > 0),
+    weight integer NOT NULL CHECK (weight > 0),
+    max_speed integer NOT NULL CHECK (max_speed > 0),
+    max_load integer NOT NULL CHECK (max_load > 0),
+    year smallint NOT NULL CHECK (year > 2000)
 );
 
 CREATE TYPE scooter_status AS ENUM ('enabled', 'disabled');
 
 CREATE TABLE IF NOT EXISTS scooters (
-    id uuid primary key default uuid_generate_v4(),
-    model_id uuid not null references scooter_models(id),
-    status scooter_status not null default 'disabled',
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    model_id uuid NOT NULL REFERENCES scooter_models(id),
+    status scooter_status NOT NULL DEFAULT 'disabled',
     image_link text,
-    number varchar(8) not null
+    number varchar(8) NOT NULL
 );
 
 CREATE TYPE scooter_lock_state AS ENUM ('locked', 'unlocked');
 CREATE TYPE scooter_lights_state AS ENUM ('on', 'off');
 
 CREATE TABLE IF NOT EXISTS pings (
-    scooter_id uuid not null references scooters(id),
-    date timestamp not null default now(),
+    scooter_id uuid NOT NULL REFERENCES scooters(id),
+    date timestamp NOT NULL DEFAULT now(),
     meta_info json,
-    location geography not null,
-    battery_level smallint not null check (battery_level >= 0 AND battery_level <= 100),
-    lock_state scooter_lock_state not null,
-    lights_state scooter_lights_state not null
+    location geography NOT NULL,
+    battery_level smallint NOT NULL CHECK (battery_level >= 0 AND battery_level <= 100),
+    lock_state scooter_lock_state NOT NULL,
+    lights_state scooter_lights_state NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS rentals (
-    id uuid primary key default uuid_generate_v4(),
-    user_id uuid not null references users(id),
-    scooter_id uuid not null references scooters(id),
-    start_price integer not null check (start_price >= 0),
-    per_minute_price integer not null check (per_minute_price >= 0),
-    date_started timestamp not null default now(),
-    date_finished timestamp check (date_finished > date_started)
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id uuid NOT NULL REFERENCES users(id),
+    scooter_id uuid NOT NULL REFERENCES scooters(id),
+    start_price integer NOT NULL CHECK (start_price >= 0),
+    per_minute_price integer NOT NULL CHECK (per_minute_price >= 0),
+    date_started timestamp NOT NULL DEFAULT now(),
+    date_finished timestamp CHECK (date_finished > date_started)
 );
 
 CREATE TABLE IF NOT EXISTS bookings (
-    id uuid primary key default uuid_generate_v4(),
-    user_id uuid not null references users(id),
-    scooter_id uuid not null references scooters(id),
-    date_started timestamp not null default now(),
-    date_finished timestamp check (date_finished > date_started)
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id uuid NOT NULL REFERENCES users(id),
+    scooter_id uuid NOT NULL REFERENCES scooters(id),
+    date_started timestamp NOT NULL DEFAULT now(),
+    date_finished timestamp CHECK (date_finished > date_started)
 );
 
 CREATE TABLE IF NOT EXISTS parkings (
-    id uuid primary key default uuid_generate_v4(),
-    location geography not null
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    location geography NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS restricted_zones (
-    id uuid primary key default uuid_generate_v4(),
-    polygon geometry not null,
-    speed_limit smallint not null check (speed_limit >= 0)
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    polygon geometry NOT NULL,
+    speed_limit smallint NOT NULL CHECK (speed_limit >= 0)
 );
 
 CREATE TABLE IF NOT EXISTS totp (
-    code integer not null,
-    date_sent timestamp not null default now(),
-    phone text not null,
-    signature text unique not null,
-    date_used timestamp check (date_used > date_sent)
+    code integer NOT NULL,
+    date_sent timestamp NOT NULL DEFAULT now(),
+    phone text NOT NULL,
+    signature text UNIQUE NOT NULL,
+    date_used timestamp CHECK (date_used > date_sent)
 );
 
 CREATE TABLE IF NOT EXISTS auth_tokens (
-    user_id uuid not null references users(id),
-    value text unique not null,
-    date_expired timestamp not null
+    user_id uuid NOT NULL REFERENCES users(id),
+    value text UNIQUE NOT NULL,
+    date_expired timestamp NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS settings (
-    name varchar(64) unique not null,
-    value text not null
+    name varchar(64) UNIQUE NOT NULL,
+    value text NOT NULL
 );
