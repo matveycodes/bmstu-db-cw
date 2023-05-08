63528028197295ac4e99b2cd68cdcbef20a8ba76
Matvey Kottsov <contact@matvey.codes>
2023-05-08 21:59:02 +0300

    feat: design section finished

--- docs/inc/lst/init.sql
+++ docs/inc/lst/init.sql
@@ -4,14 +4,14 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TYPE user_status AS ENUM ('pending', 'active', 'blocked');
 
-CREATE TYPE user_role AS ENUM ('customer', 'admin');
+CREATE TYPE user_role AS ENUM ('customer', 'technician', 'admin');
 
 CREATE DOMAIN phone_number AS TEXT CHECK (VALUE ~ '^7[0-9]{10}$');
 
 CREATE TABLE IF NOT EXISTS users (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     status user_status NOT NULL DEFAULT 'pending',
-    date_joined timestamp NOT NULL DEFAULT now() CHECK (date_joined > birthdate),
+    date_joined timestamp with time zone NOT NULL DEFAULT now() CHECK (date_joined > birthdate),
     middle_name varchar(256),
     first_name varchar(256),
     last_name varchar(256),
@@ -51,7 +51,7 @@ CREATE TYPE scooter_lights_state AS ENUM ('on', 'off');
 
 CREATE TABLE IF NOT EXISTS pings (
     scooter_id uuid NOT NULL REFERENCES scooters(id),
-    date timestamp NOT NULL DEFAULT now(),
+    date timestamp with time zone NOT NULL DEFAULT now(),
     meta_info json,
     location geography NOT NULL,
     battery_level smallint NOT NULL CHECK (battery_level >= 0 AND battery_level <= 100),
@@ -65,16 +65,16 @@ CREATE TABLE IF NOT EXISTS rentals (
     scooter_id uuid NOT NULL REFERENCES scooters(id),
     start_price integer NOT NULL CHECK (start_price >= 0),
     per_minute_price integer NOT NULL CHECK (per_minute_price >= 0),
-    date_started timestamp NOT NULL DEFAULT now(),
-    date_finished timestamp CHECK (date_finished > date_started)
+    date_started timestamp with time zone NOT NULL DEFAULT now(),
+    date_finished timestamp with time zone CHECK (date_finished > date_started)
 );
 
 CREATE TABLE IF NOT EXISTS bookings (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_id uuid NOT NULL REFERENCES users(id),
     scooter_id uuid NOT NULL REFERENCES scooters(id),
-    date_started timestamp NOT NULL DEFAULT now(),
-    date_finished timestamp CHECK (date_finished > date_started)
+    date_started timestamp with time zone NOT NULL DEFAULT now(),
+    date_finished timestamp with time zone NOT NULL CHECK (date_finished > date_started)
 );
 
 CREATE TABLE IF NOT EXISTS parkings (
@@ -90,19 +90,34 @@ CREATE TABLE IF NOT EXISTS restricted_zones (
 
 CREATE TABLE IF NOT EXISTS totp (
     code integer NOT NULL,
-    date_sent timestamp NOT NULL DEFAULT now(),
+    date_sent timestamp with time zone NOT NULL DEFAULT now(),
     phone text NOT NULL,
     signature text UNIQUE NOT NULL,
-    date_used timestamp CHECK (date_used > date_sent)
+    date_used timestamp with time zone CHECK (date_used > date_sent)
 );
 
 CREATE TABLE IF NOT EXISTS auth_tokens (
     user_id uuid NOT NULL REFERENCES users(id),
     value text UNIQUE NOT NULL,
-    date_expired timestamp NOT NULL
+    date_expired timestamp with time zone NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS settings (
     name varchar(64) UNIQUE NOT NULL,
     value text NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS subscriptions (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    title varchar(64) NOT NULL,
+    duration integer NOT NULL CHECK (duration > 0),
+    price integer NOT NULL CHECK (price >= 0)
+);
+
+CREATE TABLE IF NOT EXISTS purchased_subscriptions (
+    subscription_id uuid NOT NULL REFERENCES subscriptions(id),
+    user_id uuid NOT NULL REFERENCES users(id),
+    date_started timestamp with time zone NOT NULL DEFAULT now(),
+    date_finished timestamp with time zone NOT NULL CHECK (date_finished > date_started),
+    date_purchased timestamp with time zone NOT NULL DEFAULT now()
 );
